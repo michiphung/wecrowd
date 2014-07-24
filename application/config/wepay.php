a10bdbd1535a01a1217a27714e19d5d5f4ae9e06
Michelle Phung <michelle.phung@wepay.com>
2014-07-24 11:22:49 -0700

    Changing Client Id and Secret

--- application/config/wepay.php
+++ application/config/wepay.php
@@ -1,12 +1,12 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-WePay::useStaging('136527','4e01dbaabb', '2014-01-08');
+WePay::useStaging('65452','37e3a127fd', '2014-01-08');
 
 return array(
 
         // Your Client ID
-	'client_id'       => '136527',
+	'client_id'       => '65452',
 
         // Your Client Secret
-	'client_secret'   => '4e01dbaabb',
+	'client_secret'   => '37e3a127fd',
 );
