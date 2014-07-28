00327d9d8450ea4b6154c5bbfe63f24901de0998
Michelle Phung <michelle.phung@wepay.com>
2014-07-28 12:54:40 -0700

    fixing link

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,7 +6,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="brand" href="/"><img src="http://www.wecrowd.wepay-inc.com/content/img/weCrowd_logo_small.png"></a>
+          <a class="brand" href="/"><img src="http://www.wecrowd.wepay-inc.com/content/img/wecrowd_logo_small.png"></a>
           <div class="nav-collapse collapse">
             <ul class="nav pull-right">
               <li class="active"><a href="<? echo url::base().'user/register' ?>">Register</a></li>
