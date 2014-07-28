98d63424ed240eda0cd783563a046693b66ef1e2
Michelle Phung <michelle.phung@wepay.com>
2014-07-28 14:56:33 -0700

    fixing manage page

--- application/views/user/manage.php
+++ application/views/user/manage.php
@@ -3,12 +3,15 @@
 <head>
 	<h1><font size="6"><center>Welcome to WeCrowd. <br> Please manage your funds. </center></font></h1>
 	<br>
+	<br>
+	<hr width="10%" size =10>
 	<style>
 	</style>
 </head>	
         <body>
         	<center>
         	<a href="<?=$wepay_link?>">View your funds</a>
+        	<hr>
         	<div id="kyc_div"><div>
 				<script type="text/javascript" src="https://www.wepay.com/min/js/iframe.wepay.js">
 				</script>
