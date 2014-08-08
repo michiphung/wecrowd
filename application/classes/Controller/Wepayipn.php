df908cf7d656d16319afe23d06824dd09b7de1a1
Michelle Phung <michelle.phung@wepay.com>
2014-08-08 14:17:40 -0700

    wepay ipn

--- application/classes/Controller/Wepayipn.php
+++ application/classes/Controller/Wepayipn.php
@@ -1,18 +1,13 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Wepayipn extends Controller_Base {
+class Controller_Wepayipn extends Controller{
 
 	public function action_index() {
-		if (Auth::instance()->logged_in()) {
-			$config = Kohana::$config->load('wepay');
-            $user = Auth::instance()->get_user();
-            $campaign = ORM::factory('campaign')->where('email', '=', $user->email)->find();
-            $this->template->content = View::factory('user/account_summary');
-			if (!empty($_POST['account_id'])) {
+        if (!empty($_POST['account_id'])) {
 				$account_id= $_POST['account_id'];
-				echo 'there is a post';
-			}
-			$wepay = new WePay($campaign->getAccessToken());
+		}
+        $campaign = ORM::factory('campaign')->where('wepay_account_id', '=', $account_id)->find();
+        $wepay = new WePay($campaign->getAccessToken());
         	try {
            		 $response = $wepay->request('account', array(
                 	    'account_id'          => $account_id
@@ -21,18 +16,11 @@ class Controller_Wepayipn extends Controller_Base {
             	echo $e->getMessage();
         	}
 
-
-            $balance = $response->balances;
-            $this->template->content->balance = $balance[0]->balance;
-            $this->template->content->state = $response->state;
-
-            $campaign->balance = $balance[0]->balance;
+            // $balance = $response->balances;
+            // $campaign->balance = $balance[0]->balance;
             $campaign->state = $response->state;
- 
-    	} else {
-    		$this->template->content = "Not logged in!";
+            $campaign->save();
     	}
 	}
-}
 
 
