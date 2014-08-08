7df150919ba87f0018e0012981bab750bcd39677
Michelle Phung <michelle.phung@wepay.com>
2014-08-08 16:38:11 -0700

    testing

--- application/views/user/account.php
+++ application/views/user/account.php
@@ -34,6 +34,9 @@
     echo "<a href=$wepay_link?>Payment account</a></p>";
     echo "Account Summary</p>";
     if ($token) {
+    	if ($state = "action_required") {
+    		echo "Your profile is not fully completed! Please <a href=" . URL::base(). "user/manage>manage</a> your account</p>"
+    	}
     	echo "State: " . $state;
     	echo "Balance: $" . $balance;
     }
