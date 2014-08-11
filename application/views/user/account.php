4d1772a713f34240d193056ff9edc188c2278c70
Michelle Phung <michelle.phung@wepay.com>
2014-08-11 13:20:50 -0700

    switching active and pending

--- application/views/user/account.php
+++ application/views/user/account.php
@@ -36,10 +36,10 @@
     if ($token) {
     	if ($state == "action_required") {
     		echo "Your profile is not fully completed! Please <a href=" . URL::base(). "user/manage>manage</a> your account</p>";
-    	} else if ($state = "pending") {
-    		echo "You still have not confirmed your account. Click <a href=" . URL::base(). "user/resend_email>here</a> to resend your confirmation email </p>";
-    	} else {
+    	} else if ($state == "active") {
     		echo "Your account is completed and ready to receive payments!";
+    	} else {
+    		echo "You still have not confirmed your account. Click <a href=" . URL::base(). "user/resend_email>here</a> to resend your confirmation email </p>";
     	}
     	echo "State: " . $state . "</p>";
     	// echo "Balance: $" . $balance;
