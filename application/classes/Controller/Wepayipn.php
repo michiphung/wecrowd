ea2d8471ce9a7b716861fa4ee658dcc53359d357
Michelle Phung <michelle.phung@wepay.com>
2014-08-11 11:27:05 -0700

    changing base

--- application/classes/Controller/Wepayipn.php
+++ application/classes/Controller/Wepayipn.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Wepayipn extends Controller{
+class Controller_Wepayipn extends Controller_Base{
 
 	public function action_index() {
         if (!empty($_POST['account_id'])) {
