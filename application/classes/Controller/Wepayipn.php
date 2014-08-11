2a3a6138620924359a5711922b7f604e271f6d74
Michelle Phung <michelle.phung@wepay.com>
2014-08-11 11:30:35 -0700

    testing

--- application/classes/Controller/Wepayipn.php
+++ application/classes/Controller/Wepayipn.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Wepayipn extends Controller_Base{
+class Controller_Wepayipn extends Controller_Base {
 
 	public function action_index() {
         if (!empty($_POST['account_id'])) {
@@ -10,6 +10,7 @@ class Controller_Wepayipn extends Controller_Base{
         $campaign = ORM::factory('campaign')->where('wepay_account_id', '=', $account_id)->find();
         $wepay = new WePay($campaign->getAccessToken());
         	try {
+                 echo 1;
            		 $response = $wepay->request('account', array(
                 	    'account_id'          => $account_id
                     	));
