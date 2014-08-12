da7a792754d2efce26da74f09f8f091c8348ec12
Michelle Phung <michelle.phung@wepay.com>
2014-08-12 14:43:23 -0700

    fixing look of account

--- application/views/user/account.php
+++ application/views/user/account.php
@@ -3,27 +3,27 @@
 	<h1 class="floatLeft"><? echo $first_name?>'s Campaign</h1>
 		<p> <? echo $wepay; ?></p>
 		<p>
-		<b>Name:</b>
+		<b><font color="7acaea">Name:</font></b>
 		<? echo $first_name . ' ' . $last_name; ?>
 		</p>
 		<p>
-		<b>Email:</b>
+		<b><font color="7acaea">Email:</font></b>
 		<? echo $email; ?>
 		</p>
 
 		<p>
-		<b>Campaign:</b>
+		<b><font color="7acaea">Campaign:</font></b>
 		<? echo $campaign_name; ?>
 		</p>
 
 		<p>
-		<b>Description:</b>
+		<b><font color="7acaea">Description:</font></b>
 		<? echo $description; ?>
 
 		</p>
 
 		<p>
-		<b>Donation:</b>
+		<b><font color="7acaea">Donation:</font></b>
 		<? echo '$'.$price; ?>
 		</p>
 		<p><p>
@@ -40,16 +40,18 @@
 		<? if ($token) {
 	    	echo "<h1>Account Detail</h1></p>";
 	    	if ($state == "action_required") {
+	    		echo "<font color='7acaea'>Status:</font> Action Required";
 	    		echo "Your profile is not fully completed! Please <a href=" . URL::base(). "user/manage>manage</a> your account</p>";
 	    		echo "<a href=$wepay_link?>Payment account</a></p>";
 	    	} else if ($state == "active") {
+	    		echo "<font color='7acaea'>Status:</font> Active";
 	    		echo "Your account is completed and ready to receive payments!</p>";
-	    		echo "<a href=$wepay_link?>Payment account</a></p>";
+	    		echo "You can view your payment account <a href=$wepay_link?>here</a></p>.";
 	    	} else {
+	    		echo "<font color='7acaea'>Status:</font> Pending";
 	    		echo "You still have not confirmed your account.<br>Click <a href=" . URL::base(). "user/resend_email>here</a> to resend your confirmation email. </p>";
 	    	}
-	    	echo "State: " . $state . "</p>";
-	    	echo "Balance: $" . $balance;
+	    	echo "<font color='7acaea'>Balance:</font> $" . $balance;
 	    }
 	    ?>
     </div>
