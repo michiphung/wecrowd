c7aeadeaf23c471984bf182a0de1b60b14fa6cd2
Michelle Phung <michelle.phung@wepay.com>
2014-08-13 16:10:37 -0700

    testing demo

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -6,7 +6,7 @@
   <h1>Welcome to WeCrowd</h1>
     <p>You can browse different fundraising campaign. If you want to start your own campaign, please register.</p>
 
-    <a href="javascript:void(0);" onclick="javascript:introJs().start();" class="btn btn-demo btn-large" id='buy-now-button'>Demo as Merchant</a>
+     <a href="javascript:void(0);" onclick="startIntro();" class="btn btn-demo btn-large">Demo as Merchant</a>
 
      <script type="text/javascript">
       function startIntro(){
