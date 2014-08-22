51c74d98ba8e5981472c6669e38c8ff6d0ffac56
Michelle Phung <michelle.phung@wepay.com>
2014-08-22 15:15:56 -0700

    removing auto fill for registration

--- application/views/user/add_admin.php
+++ application/views/user/add_admin.php
@@ -1,24 +1,25 @@
+
 <h1>Admin and Campaign registration</h1>
 <?php echo Form::open('user/complete_admin_registration') ?>
 
   <div class="field">
     <label for="campaign_first_name">First Name</label>
-    <?php echo Form::input('first_name', $user['first_name'],array('id' => 'first_name', 'readonly' => true)) ?>
+    <?php echo Form::input('first_name', '',array('id' => 'first_name') ?>
   </div>
 
    <div class="field">
     <label for="campaign_last_name">Last Name</label>
-    <?php echo Form::input('last_name',$user['last_name'],array('id' => 'last_name', 'readonly' => true)) ?>
+    <?php echo Form::input('last_name','',array('id' => 'last_name') ?>
   </div>
 
   <div class="field">
     <label for="campaign_campaign_name">Campaign Name</label>
-    <?php echo Form::input('campaign_name',$user['campaign_name'],array('id' => 'campaign_name', 'readonly' => true)) ?>
+    <?php echo Form::input('campaign_name','',array('id' => 'campaign_name') ?>
   </div>
 
   <div class="field">
     <label for="campaign_description">Campaign Descripion</label>
-    <?php echo Form::input('description',$user['campaign_description'],array('id' => 'description', 'readonly' => true)) ?>
+    <?php echo Form::input('description',$user['campaign_description'],array('id' => 'description') ?>
   </div>
 
     <div id="registration_wrap" class="field" >
